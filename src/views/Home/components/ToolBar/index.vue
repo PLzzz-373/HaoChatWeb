5178c58543552759f4772b52d6255bb0b715346c
PLzzz-373 <1292885426@qq.com>
2024-03-16 23:28:57 +0800

    基本聊天页面

--- src/views/Home/components/ToolBar/index.vue
+++ src/views/Home/components/ToolBar/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import { ChatLineRound } from '@element-plus/icons-vue'
-import { ElIcon } from 'element-plus'
+import { ElAvatar, ElIcon } from 'element-plus'
 </script>
 
 <template>
   <aside class="side_toolbar">
-    <el-icon><ChatLineRound /></el-icon>
+    <ElAvatar
+      size="large"
+      src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+    />
+    <i class="icon-github" />
   </aside>
 </template>
 
